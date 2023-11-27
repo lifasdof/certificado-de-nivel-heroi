6311d99751b5d7fc2f590f54683f011a832cc3f6
lifasdof <lmacena57@gmail.com>
2023-11-26 21:43:29 -0300

    primeiro commit

---
+++ if-else-elseif.js
@@ -0,0 +1,20 @@
+let NickName= "lifasdof"
+let xp = 9
+
+if (xp < 1000){
+    console.log(NickName,"seu elo é igual ferro")
+}else if (xp >= 1001 && xp<=2000){
+    console.log(NickName,"seu elo é igual bronze")
+}else if (xp>= 2001 && xp<=5000){
+    console.log(NickName,"seu elo é igual prata")
+}else if (xp >= 6001&&xp<= 7000){
+    console.log(NickName,"seu elo é igual ouro")
+}else if (xp >= 7001&&xp <=8000){
+    console.log(NickName,"seu elo é igual platina")
+}else if (xp>=8001&& xp<=9000){
+    console.log(NickName,"seu elo é igual a acendente")
+}else if (xp>=9001 && xp<=10000){
+    console.log(NickName,"seu elo é igual a imortal")
+}else if (xp>=10001){
+    console.log(NickName,"seu elo é igual a radiante")
+}
